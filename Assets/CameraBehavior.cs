6cf61b9e63634dfe593a5056db12ec1fc9c1cb5c
looputer <luyichen_2013@163.com>
2024-11-02 20:44:36 +0800

    Camera Follow Script Commit

---
+++ Assets/CameraBehavior.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset = new Vector3(15.61f, 2.44f, 2.23f);
+
+    void Start()
+    {
+        target = GameObject.Find("Player").transform;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = target.TransformPoint(offset);
+        transform.LookAt(target);
+    }
+}
