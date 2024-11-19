c4badba1a19cbed88e7aec4508acffe2b7054e2e
looputer <luyichen_2013@163.com>
2024-11-19 20:05:55 +0800

    Script Commit

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -4,12 +4,10 @@ public class CameraBehaviour : MonoBehaviour
 {
     public Transform target;
     public Vector3 offset = new Vector3(15.61f, 2.44f, 2.23f);
-
     void Start()
     {
         target = GameObject.Find("Player").transform;
     }
-
     void LateUpdate()
     {
         transform.position = target.TransformPoint(offset);
---
+++ Assets/Scripts/EmeryBehaviour.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+
+public class EmeryBehaviour : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+                
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
